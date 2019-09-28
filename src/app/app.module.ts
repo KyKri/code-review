f3333596650be07c1df5b409db6f46298852c0db
Kyle Krick <krkrick@gmail.com>
2019-09-28 11:26:21 -0700

    added topics list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { TopicsListComponent } from './topics-list/topics-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    TopicsListComponent
   ],
   imports: [
     BrowserModule,
