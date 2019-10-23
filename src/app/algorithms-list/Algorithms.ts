20b93e93c607b6e68435767b87ccc71321906b73
Kyle Krick <krkrick@gmail.com>
2019-10-22 23:02:06 -0700

    added algorithm mock data

---
+++ src/app/algorithms-list/Algorithms.ts
@@ -0,0 +1,34 @@
+import { IAlgorithm } from './IAlgorithm';
+
+const Algorithms: IAlgorithm[] = [
+    {
+        name: 'Bubble Sort',
+        description: '',
+        runtimeComplexities: {
+            best: '',
+            average: '',
+            worst: ''
+        },
+        spaceComplexity: '',
+    },
+    {
+        name: 'Merge Sort',
+        description: '',
+        runtimeComplexities: {
+            best: '',
+            average: '',
+            worst: ''
+        },
+        spaceComplexity: '',
+    },
+    {
+        name: 'Quicksort',
+        description: '',
+        runtimeComplexities: {
+            best: '',
+            average: '',
+            worst: ''
+        },
+        spaceComplexity: '',
+    }
+];
