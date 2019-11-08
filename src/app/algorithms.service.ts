38f928b1a60bd2b9c427f0c1a159aa8de3194c57
Kyle Krick <krkrick@gmail.com>
2019-11-07 20:54:46 -0800

    added getById, add, update and delete methods

--- src/app/algorithms.service.ts
+++ src/app/algorithms.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IAlgorithm } from './IAlgorithm';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,21 @@ export class AlgorithmsService {
 
   public getAlgorithms() {
     return this.http.get('http://localhost:4000/algorithms');
+  }
+
+  public getAlgorithmById(id: string) {
+    return this.http.get('http://localhost:4000/algorithms/' + id);
+  }
+
+  public addAlgorithm(alg: IAlgorithm) {
+    return this.http.post('http://localhost:4000/algorithms/add', alg);
+  }
+
+  public updateAlgorithm(id: string, alg: IAlgorithm) {
+    return this.http.post('http://localhost:4000/algorithms/update/' + id, alg);
+  }
+
+  public deleteAlgorithm(id: string) {
+    return this.http.get('http://localhost:4000/algorithms/delete/' + id);
   }
 }
