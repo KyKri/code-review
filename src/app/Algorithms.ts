9b0978c36eb7a60cc201cd66d89c28898db8b207
Kyle Krick <krkrick@gmail.com>
2019-11-16 08:37:16 -0800

    broke up rather long lines in test data

--- src/app/Algorithms.ts
+++ src/app/Algorithms.ts
@@ -19,7 +19,8 @@ export const algorithms: IAlgorithm[] = [
         name: 'Mergesort',
         description: `Conceptually, a merge sort works as follows:
          1. Divide the unsorted list into n sublists, each containing one element (a list of one element is considered sorted).
-         2. Repeatedly merge sublists to produce new sorted sublists until there is only one sublist remaining. This will be the sorted list.`,
+         2. Repeatedly merge sublists to produce new sorted sublists until there is only one sublist remaining.
+         This will be the sorted list.`,
         runtimeComplexities: {
             best: 'O(n log(n))',
             average: 'O(n log(n))',
@@ -30,7 +31,8 @@ export const algorithms: IAlgorithm[] = [
     {
         _id: '',
         name: 'Quicksort',
-        description: `Quicksort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined.
+        description: `Quicksort is a comparison sort, meaning that it can sort items of any type for which a "less-than"
+         relation (formally, a total order) is defined.
          Efficient implementations of Quicksort are not a stable sort, meaning that the relative order of equal sort items is not preserved.
          Quicksort can operate in-place on an array, requiring small additional amounts of memory to perform the sorting.
          It is very similar to selection sort, except that it does not always choose worst-case partition.`,
